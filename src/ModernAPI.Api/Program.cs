180e9b9e1fd094272d0bfd4104352433af742344
Daspudding7 <chatelain777@gmail.com>
2025-07-03 17:42:00 +0200

    changed api project to dotnet 8

--- src/ModernAPI.Api/Program.cs
+++ src/ModernAPI.Api/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ModernAPI.DataAccess.Context;
 
-namespace ModernAPI
+namespace ModernAPI.Api
 {
     public class Program
     {
@@ -12,6 +12,9 @@ namespace ModernAPI
             // Add services to the container.
 
             builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             builder.Services.AddDbContext<UserContext>(options =>
                 options.UseMySql(
@@ -23,6 +26,11 @@ namespace ModernAPI
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
 
             app.MapControllers();
 
---
+++ src/ModernAPI/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ModernAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("users")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
